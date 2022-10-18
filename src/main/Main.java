7bc69a73926713d9978b899b435aebe800dcee10
FARHAD  NAJAFOV <farhadnajafov98.com>
2022-10-18 12:54:31 +0400

    Numbers updated

--- src/main/Main.java
+++ src/main/Main.java
@@ -19,16 +19,36 @@ public class Main {
         JButton b7=new JButton("7");
         JButton b8=new JButton("8");
         JButton b9=new JButton("9");
+        JButton b0=new JButton("0");
+        JButton bdot=new JButton(".");
+        JButton bclear=new JButton("CLEAR");
+        JButton bper=new JButton("%");
+        JButton bplus=new JButton("+");
+        JButton bminus=new JButton("-");
+        JButton bx=new JButton("x");
+        JButton bdivide=new JButton("/");
+        JButton bequal=new JButton("=");
 
-        b1.setBounds(10,81,60,60);
-        b2.setBounds(80,81,60,60);
-        b3.setBounds(150,81,60,60);
-        b4.setBounds(10,151,60,60);
-        b5.setBounds(80,151,60,60);
-        b6.setBounds(150,151,60,60);
-        b7.setBounds(10,221,60,60);
-        b8.setBounds(80,221,60,60);
-        b9.setBounds(150,221,60,60);
+
+        bclear.setBounds(10,81,130,60);
+        bper.setBounds(150,81,60,60);
+        b1.setBounds(10,151,60,60);
+        b2.setBounds(80,151,60,60);
+        b3.setBounds(150,151,60,60);
+        b4.setBounds(10,221,60,60);
+        b5.setBounds(80,221,60,60);
+        b6.setBounds(150,221,60,60);
+        b7.setBounds(10,291,60,60);
+        b8.setBounds(80,291,60,60);
+        b9.setBounds(150,291,60,60);
+        b0.setBounds(10,361,130,60);
+        bdot.setBounds(150,361,60,60);
+        bdivide.setBounds(220,81,60,60);
+        bx.setBounds(220,151,60,60);
+        bplus.setBounds(220,221,60,60);
+        bminus.setBounds(220,291,60,60);
+        bequal.setBounds(220,361,60,60);
+
 
 
         frame.setSize(400,500);
@@ -43,6 +63,16 @@ public class Main {
         frame.add(b7);
         frame.add(b8);
         frame.add(b9);
+        frame.add(b0);
+        frame.add(bdot);
+        frame.add(bper);
+        frame.add(bclear);
+        frame.add(bplus);
+        frame.add(bminus);
+        frame.add(bdivide);
+        frame.add(bx);
+        frame.add(bequal);
+
         frame.add(text);
 
     }
